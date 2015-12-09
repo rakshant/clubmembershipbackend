a78b504e4545fba2f35cfd255a4e5661e931cd89
agrawal_am <agrawal_am@Amit-Al.India.XoriantCorp.com>
2015-12-09 21:18:56 +0530

    Ruchi's RegistrationServiceImpl Added in Corrected code

--- clubmembershipbackend/src/main/java/com/cmm/spring/service/LoginServiceImpl.java
+++ clubmembershipbackend/src/main/java/com/cmm/spring/service/LoginServiceImpl.java
@@ -1,7 +1,5 @@
 package com.cmm.spring.service;
 
-
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,81 +12,72 @@ import com.cmm.spring.mongo.collections.UserLogin;
 import com.cmm.spring.mongo.collections.UserRegistration;
 import com.cmm.spring.rest.repository.LoginRepository;
 import com.cmm.spring.rest.repository.RegistrationRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class LoginServiceImpl implements LoginService {
-	
-	
+
 	@Autowired
 	private LoginRepository loginRepository;
-	
+
 	@Autowired
 	private RegistrationRepository registrationRepository;
-	
+
 	@Autowired
 	private MongoOperations mongoOperation;
-	
-	
-	 List<UserRegistration> userList;
 
-	public String loginUser(UserLogin userLogin) throws JsonProcessingException {
-		String response;
-		Query query = new Query();	
-		ObjectMapper mapper=new ObjectMapper();
-		
-		if(userLogin.getEmailId()!=null){
-		
-		query.addCriteria(Criteria.where("emailId").regex(userLogin.getEmailId()));
-		
-		}
-		
-		else 
-			response="{\"response\":\"failed\"}";
-		
-		if(userLogin.getPassword()!=null){
-		
-		query.addCriteria(Criteria.where("password").regex(userLogin.getPassword()));
-		
-		}
-		else response="{\"response\":\"failed\"}";
-		
-		userList=mongoOperation.find(query, UserRegistration.class);
-		
-		if(userList.size()!=0){
-		
-		loginRepository.insert(userLogin);
-		
-		response= "success";
-		
-		}
-		
-		response= "{\"response\":\"failed\"}";
-		
-		
-		String loginJson=mapper.writeValueAsString(response);
-		return loginJson;
-		 
-	}
+	List<UserRegistration> registrationCheckList;
+	List<UserRegistration> loggedInUserList;
 
+	public String loginUser(UserLogin userLogin) {
 
-	/*public void delete(String emailId) {
-		
-		List<UserLogin> temp_users=registrationRepository.findAll();
-		Iterator<UserRegistration> users_iterator=temp_users.iterator();
-		while(users_iterator.hasNext())
-		{
-			UserRegistration user=users_iterator.next();
-			if(user.getUserName().equals(userName))
-			{
-				registrationRepository.delete(user.getId());
+		Query query = new Query();
+
+		if (userLogin.getEmailId() != null) {
+
+			query.addCriteria(Criteria.where("emailId").regex(
+					userLogin.getEmailId()));
+
+		} else
+			return "failed";
+
+		if (userLogin.getPassword() != null) {
+
+			query.addCriteria(Criteria.where("password").regex(
+					userLogin.getPassword()));
+
+		} else
+			return "failed";
+
+		loggedInUserList = mongoOperation.find(query, UserRegistration.class);
+
+		if (loggedInUserList.size() != 0) {
+			loginRepository.insert(userLogin);
+			for (UserRegistration user : loggedInUserList) {
+				return user.getUserType();
 			}
 		}
-		
-	}*/
+		return "failed";
+
+	}
+
+	/*
+	 * public void delete(String emailId) {
+	 * 
+	 * List<UserLogin> temp_users=registrationRepository.findAll();
+	 * Iterator<UserRegistration> users_iterator=temp_users.iterator();
+	 * while(users_iterator.hasNext()) { UserRegistration
+	 * user=users_iterator.next(); if(user.getUserName().equals(userName)) {
+	 * registrationRepository.delete(user.getId()); } }
+	 * 
+	 * }
+	 */
 
 }
+
+
+
+
+
 
 
 
