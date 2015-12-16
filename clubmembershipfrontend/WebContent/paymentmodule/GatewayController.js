b1ed59413a26c8824e3c27c05bfd721760db8de2
rakshant <rakshantmishra@gmail.com>
2015-12-16 20:48:22 +0530

    gateway working....

--- clubmembershipfrontend/WebContent/paymentmodule/GatewayController.js
+++ clubmembershipfrontend/WebContent/paymentmodule/GatewayController.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp', []);
 app.controller('dataCtrl', function($scope,$http) {
-	console.log("----------------------------->");
+
 	
 	var str=location.search.split('&');		
 	$scope.amount =parseInt(str[1].substring(4));
@@ -12,11 +12,15 @@ app.controller('dataCtrl', function($scope,$http) {
 	$scope.occupation=str[7].substring(11);
 	$scope.email=str[6].substring(7);
 	
-	console.log("----->"+$scope.firstName)
+	
 	
 	
 	$scope.proceed=function(data){
 		window.open('http://localhost:8089/clubmembershipfrontend/paymentmodule/paymentModule.html?id='
 				+str[0].substring(4)+"&fee="+str[1].substring(4)+"&type="+str[2].substring(5),'_self');
 	}		
+	
+	$scope.cancel=fucntion(){
+		window.close();
+	}
 });
